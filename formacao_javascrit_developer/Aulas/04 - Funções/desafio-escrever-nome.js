6321bcc5c56352417dcc475d45035fb6dec1eb0c
Walessonrdreis <wallllase@hotmail.com>
2024-01-03 18:14:06 -0300

    update: add funções

--- formacao_javascrit_developer/Aulas/04 - Funções/desafio-escrever-nome.js
+++ formacao_javascrit_developer/Aulas/04 - Funções/desafio-escrever-nome.js
@@ -1,6 +1,32 @@
+//função escrever meu nome
 function writeYourName(name){
 return 'meu nome é '+  name;
 }
+//Função classificar idade
+function calcularIdadePorAnoDeNascimento(anoNascimento){
+    const dataAtual = new Date();
+    const anoAtual = dataAtual.getUTCFullYear();
+
+    return  anoAtual - anoNascimento;
 
 
-console.log(myName('walesson ReisBa'));
+}
+function classificarIdade(idade){
+    if (idade <= 17){
+        return writeYourName('walesson ReisBa') + 'Tenho '+ idade +'. Sou Menor de Idade'
+    }else if (idade >= 18 & idade < 60){
+        return writeYourName('walesson ReisBa') + ' Tenho '+ idade + ' anos. Sou Adulto'
+    }else{
+            return writeYourName('walesson ReisBa') +' Tenho'+ idade + '. Sou um Coroa'
+    }
+    
+}
+
+
+
+
+(function (){
+    const idade = calcularIdadePorAnoDeNascimento(1994);
+    const idadeClassificada= classificarIdade(idade);
+    console.log(idadeClassificada);
+})();
