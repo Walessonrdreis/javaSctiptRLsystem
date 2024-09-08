5c7d3637a2f55ca35a20d4880de595de7e0a57c9
Walessonrdreis <wallllase@hotmail.com>
2024-09-08 18:06:09 -0300

    @new dir DEVMEDIA

---
+++ DEVMEDIA/Estruturas_repeticao.js
@@ -0,0 +1,32 @@
+/*
+*While
+ *
+let capital_emprestado = 3000;
+let taxas_juros = 0.02;
+
+let contador = 1;
+let parcelas_totais = 5;
+
+while(contador <= parcelas_totais) {
+    let numero_parcela = contador;
+    let juros_emprestimo = capital_emprestado * taxas_juros * numero_parcela;
+
+    console.log("A taxa de juros no periode de " + numero_parcela + " mes(es) é de R$" + juros_emprestimo + ",00");
+    contador++;
+}
+*/
+
+/* let paises = ["Brasil", "Suíça", "França", "Japão"];
+
+let contador = 0;
+
+while ( contador < paises.length ) {
+
+    console.log(paises[contador]);
+    contador++;
+
+} */
+const idade =24;
+const novaIdade = idad + 1;
+window.fetch;
+
