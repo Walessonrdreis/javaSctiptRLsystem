fc51e60c1cd4774c6024eb3e2385d92c69479aa5
Walessonrdreis <wallllase@hotmail.com>
2024-09-12 05:05:58 -0300

    refactoring function test

--- javascript_assertivo_tests/projetos/01-fundamentos-de-testes/02-frameworks-e-ambientes/mini-framework-com-assercies.js
+++ javascript_assertivo_tests/projetos/01-fundamentos-de-testes/02-frameworks-e-ambientes/mini-framework-com-assercies.js
@@ -1,10 +1,40 @@
 // Asserção de test
 const checkThat = (value) =>{
     // Precisa retorna um objeto
-        const assertions = {};
+        const assertions = {
+
+            //função ehExaamenteIgualA
+            andExactTheSameAs(esperado){
+                if  (valor  ===  esperado)  {
+
+                    console .log(`✅ ${titulo} Teste  passou`);
+            
+              }  else  {
+            
+                    console .error(`🚨 ${titulo} Ih,  deu  ruim...`);
+            
+              }
+
+            }
+        };
 
         return assertions;
 
 };
+//Refatoração da  função teste 
+const  teste  =  (titulo,  esperado,  retornado)  =>  {
+
+    if  (esperado  ===  retornado)  {
+
+        console .log(`✅ ${titulo} Teste  passou`);
+
+  }  else  {
+
+        console .error(`🚨 ${titulo} Ih,  deu  ruim...`);
+
+  }
+
+}
+
 
 
